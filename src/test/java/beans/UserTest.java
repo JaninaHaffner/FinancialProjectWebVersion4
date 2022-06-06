0d6ceb3f46802e900bc64e7751f544fd7e653464
Janina <janinahaffner@gmail.com>
2022-06-06 11:44:16 +0200

    version 4 added unit tests

---
+++ src/test/java/beans/UserTest.java
@@ -0,0 +1,82 @@
+package beans;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getUsername() {
+    }
+
+    @Test
+    void setUsername() {
+    }
+
+    @Test
+    void getPassword() {
+    }
+
+    @Test
+    void setPassword() {
+    }
+
+    @Test
+    void getEmail() {
+    }
+
+    @Test
+    void setEmail() {
+    }
+
+    @Test
+    void getFullname() {
+    }
+
+    @Test
+    void setFullname() {
+    }
+
+    @Test
+    void getPreferences() {
+    }
+
+    @Test
+    void setPreferences() {
+    }
+
+    @Test
+    void getUpdates() {
+    }
+
+    @Test
+    void setUpdates() {
+    }
+
+    @Test
+    void getStockExchange() {
+    }
+
+    @Test
+    void setStockExchange() {
+    }
+
+    @Test
+    void getSymbols() {
+    }
+
+    @Test
+    void setSymbols() {
+    }
+}
---
+++ src/test/java/com/financialprojectwebversion4/UserRegistrationServletTest.java
@@ -0,0 +1,114 @@
+package com.financialprojectwebversion4;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserRegistrationServletTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void doGet() {
+    }
+
+    @Test
+    void getLastModified() {
+    }
+
+    @Test
+    void doHead() {
+    }
+
+    @Test
+    void doPost() {
+    }
+
+    @Test
+    void doPut() {
+    }
+
+    @Test
+    void doDelete() {
+    }
+
+    @Test
+    void doOptions() {
+    }
+
+    @Test
+    void doTrace() {
+    }
+
+    @Test
+    void service() {
+    }
+
+    @Test
+    void testService() {
+    }
+
+    @Test
+    void destroy() {
+    }
+
+    @Test
+    void getInitParameter() {
+    }
+
+    @Test
+    void getInitParameterNames() {
+    }
+
+    @Test
+    void getServletConfig() {
+    }
+
+    @Test
+    void getServletContext() {
+    }
+
+    @Test
+    void getServletInfo() {
+    }
+
+    @Test
+    void init() {
+    }
+
+    @Test
+    void testInit() {
+    }
+
+    @Test
+    void log() {
+    }
+
+    @Test
+    void testLog() {
+    }
+
+    @Test
+    void getServletName() {
+    }
+
+    @Test
+    void testDoGet() {
+    }
+
+    @Test
+    void testDoPost() {
+    }
+
+    @Test
+    void getHTMLString() {
+    }
+}
