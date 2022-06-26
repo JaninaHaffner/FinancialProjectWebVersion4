db4e2785eefe280126052a6d883d4187688ca117
MoipatiThoobe <86411094+MoipatiThoobe@users.noreply.github.com>
2022-06-26 19:09:41 +0200

    Add files via upload

---
+++ src/main/webapp/jsps/data.js
@@ -0,0 +1,270 @@
+const data = [{
+	stockExchange: "Currencies Trading(Forex)",
+	symbols: "EUR/USD;GBP/USD;USD/JPY;USD/CHF;AUD/USD"
+},
+{
+	stockExchange: "Commodities Trading",
+	symbols: "GC;SI;CO;CL;CR"
+},
+
+{
+	stockExchange: "Cypto",
+	symbols: "BTC;ETH;XRP;BCH;LTC;"
+},
+{
+	stockExchange: "Futures",
+	symbols: "GCQ18;CLU18;SPU18;E6Q18;ZQQ18"
+},
+{
+	stockExchange: "Bonds",
+	symbols: "US10YBY;US30YBY;GER10YBY;UK10YBY;US10YTN"
+},
+{
+	stockExchange: "USA",
+	symbols: "AAPL;MSFT;GOOG;FB;ORCL"
+},
+{
+	stockExchange: "USA OTC",
+	symbols: "DTEGY;SSDOY;SKYAY;CIOXY;WMMVY"
+},
+{
+	stockExchange: "USA OTC BB",
+	symbols: "SNGX;NEPH;CRCUF;INVS;NAOV"
+},
+{
+	stockExchange: "Japan",
+	symbols: "7203;9437;9432;7974;2914"
+},
+{
+	stockExchange: "Hong Kong",
+	symbols: "01398;00941;00857;00939;00700"
+}, 
+{
+	stockExchange: "China (Shanghai)",
+	symbols: "601398;601857;601939;601628;601988"
+},
+{
+	stockExchange: "China (Shenzhen)",
+	symbols: "200725;000100;000709;200625;002024"
+},
+{
+	stockExchange: "Germany (Frankfurt)",
+	symbols: "SIE;BTG4;SAP;BAYN;DTE"
+},
+{
+	stockExchange: "Germany (XETRA)",
+	symbols: "SIE;ALV;SAP;BAYN;DTE"
+},
+{
+	stockExchange: "Netherlands",
+	symbols: "RDSA;UNA;HEIA;INGA;MT"
+},
+{
+	stockExchange: "Belgium",
+	symbols: "ABI;SOLB;AGS;KBC;ABO"
+},
+{
+	stockExchange: "France",
+	symbols: "ALAUP"
+}, 
+{
+	stockExchange: "Portugal",
+	symbols: "BCP;EDP;BPI;JMT;GALP"
+},
+{
+	stockExchange: "Canada",
+	symbols: "RY;K;FM;BNP;ABX"
+},
+{
+	stockExchange: "Canada (Venture)",
+	symbols: "SVI;GOM;ID;LEXI;KNT"
+},
+{
+	stockExchange: "Canada CSE",
+	symbols: "TER;AHG;IAN;CRZ;MDM"
+},
+{
+	stockExchange: "Canada NEO",
+	symbols: "HALO;MGW;GGO;ETHC;MLC"
+},
+{
+	stockExchange: "Switzerland",
+	symbols: "SCHN;NWRN;UHRN;TEMN;LISN"
+},
+{
+	stockExchange: "South Korea",
+	symbols: "005930;066570;005490;021240;015760"
+},
+{
+	stockExchange: "South Korea (Kosdaq)",
+	symbols: "052710;026960;035720;051500;084990"
+},
+{
+	stockExchange: "Sweden",
+	symbols: "ERIC-A;HM-B;ATCO-A;INVE-A;NDA-SEK"
+},
+{
+	stockExchange: "Denmark",
+	symbols: "NOVO-B;MAERSK-A;DANSKE;CARL-B;VWS"
+},
+{
+	stockExchange: "Finland",
+	symbols: "NOKIA;METSA;KNEBV;ELISA;TLT1V"
+},
+{
+	stockExchange: "Iceland",
+	symbols: "ICEAIR;OSSRU;MARL;EIM;BNORDIK"
+},
+{
+	stockExchange: "India (BSE)",
+	symbols: "532540;532454;500325;500370;519475"
+}, 
+{
+	stockExchange: "India (NSE)",
+	symbols: "SBIN;LT;ICICIBANK;RELIANCE;HINDALCO"
+}, 
+{
+	stockExchange: "Taiwain",
+	symbols: "2498;2330;3605;2317;6505"
+}, 
+{
+	stockExchange: "Spain",
+	symbols: "SAN;TEF;BBVA;IBE;ELE"
+}, 
+{
+	stockExchange: "South Africa",
+	symbols: "NPN;HAR;BTI;SBK;GLN"
+}, 
+{
+	stockExchange: "Brazil",
+	symbols: "PETR4;ABEV3;ITUB3;BBDC3;BBAS3"
+}, 
+{
+	stockExchange: "New Zealand",
+	symbols: "SPK;AIA;FBU;FPH;RYM"
+}, 
+{
+	stockExchange: "Ireland",
+	symbols: "RY4C;TCO;CRG;APGN;GN1"
+}, 
+{
+	stockExchange: "Singapore",
+	symbols: "S08;U14;U11;O39;F34"
+}, 
+{
+	stockExchange: "Saudi Arabia",
+	symbols: "2010;1120;1010;7010;1090"
+}, 
+{
+	stockExchange: "Israel",
+	symbols: "TEVA;POLI;BEZQ;LUMI;ICL"
+}, 
+{
+	stockExchange: "Indonesia",
+	symbols: "TLKM;HMSP;UNVR;BBCA;ASII"
+}, 
+{
+	stockExchange: "Poland",
+	symbols: "PKO;PKN;PEO;PZU;KGH"
+}, 
+{
+	stockExchange: "Mexico",
+	symbols: "AMXL;AA;CMRB;ICA;LABB"
+}, 
+{
+	stockExchange: "Norway",
+	symbols: "DNB;STL;TEL;MHG;NHY"
+}, 
+{
+	stockExchange: "Argentina",
+	symbols: "YPFD;ALUA;COME;MIRG;SAMI"
+},
+{
+	stockExchange: "Thailand",
+	symbols: "BBL;CPF;PTTGC;KCE;TOP"
+}, 
+{
+	stockExchange: "Austria",
+	symbols: "TKA;RBI;SBO;ZAG;IIA"
+}, 
+{
+	stockExchange: "Chile",
+	symbols: "SONDA;RIPLEY;FALABELLA;BSANTANDER;LAN"
+}, 
+{
+	stockExchange: "Turkey",
+	symbols: "KCHOL;OYAYO;TRGYO;YAZIC;TOASO"
+}, 
+{
+	stockExchange: "Estonia",
+	symbols: "TAL1T;OEG1T;TVEAT;SFG1T;NCN1T"
+},
+{
+	stockExchange: "Latvia",
+	symbols: "GZE1R;LSC1R;BAL1R;OLF1R;GRD1R"
+}, 
+{
+	stockExchange: "Lithuania",
+	symbols: "ZMP1L;RSU1L;VLP1L;PZV1L;LNA1L"
+},
+{
+	stockExchange: "Philippines",
+	symbols: "GLO;BPI;SMC;FGEN;MEG"
+}, 
+{
+	stockExchange: "Abu Dhabi",
+	symbols: "ESHRAQ;ETISALAT;ALDAR;FGB;DANA"
+}, 
+{
+	stockExchange: "Dubai",
+	symbols: "DIB;ARTC;DU;MARKA;ARMX"
+}, 
+{
+	stockExchange: "Nigeria",
+	symbols: "UNITYBNK;REDSTAREX;DNMEYER;MAYBAKER;UBA"
+}, 
+{
+	stockExchange: "Colombia",
+	symbols: "ECOPETROL;GRUPOAVAL;BCOLOMBIA;GRUPOSURA;BOGOTA"
+}, 
+{
+	stockExchange: "Qatar",
+	symbols: "QNBK;KCBK;DHBK;QNCD;QEWS"
+}, 
+{
+	stockExchange: "Taipei",
+	symbols: "5820;5347;8299;3105;1565"
+},
+{
+	stockExchange: "Peru",
+	symbols: "ALICORC1;BACKUBC1;GRAMONC1;SCCO;FALABEC1"
+}, 
+{
+	stockExchange: "Egypt",
+	symbols: "ETEL;ESRS;SWDY;OCDI;OTMT"
+}, 
+{
+	stockExchange: "Greece",
+	symbols: "AEGN;TELL;EYDAP;SAR;METKK"
+}, 
+{
+	stockExchange: "Kenya",
+	symbols: "KQ;SCBK;ICDC;MSC;EABL"
+}, 
+{
+	stockExchange: "Vietnam (Hanoi)",
+	symbols: "CEO;PVI;HMH;BCC;DBC"
+},
+{
+	stockExchange: "Vietnam (Ho Chi Minh)",
+	symbols: "BID;CII;FLC;HAG;HPG"
+}, 
+{
+	stockExchange: "Czech Republic",
+	symbols: "ERBAG;CEZ;KOMB;TELEC;VIG"
+}, 
+{
+	stockExchange: "Jordan",
+	symbols: "ARBK;BOJX;JOPT;THBK;CABK"
+}
+]
